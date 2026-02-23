536ebd98e694e715d9f9c76f0bb026cdda69f165
Fumiaki Kobayashi <fumkob.dev@gmail.com>
2026-02-23 14:17:16 +0900

    🧪 Add E2E test suite bootstrap

---
+++ test/e2e/suite_test.go
@@ -0,0 +1,17 @@
+//go:build integration
+// +build integration
+
+// test/e2e/suite_test.go
+package e2e_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E Suite")
+}
